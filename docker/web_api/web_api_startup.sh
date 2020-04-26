570cf5e6f7f865514244910b1991d60f946ff0a4
Jonathan Zarecki <yonilx@gmail.com>
2020-04-26 19:17:28 +0300

    improved docker

--- docker/web_api/web_api_startup.sh
+++ docker/web_api/web_api_startup.sh
@@ -1,7 +1,5 @@
-#!/usr/bin/env bash
 cd /home/topo2vec_kavitzky/topo2vec/server_api/
-sudo apt-get update
-sudo apt-get install gnome-terminal
+apt-get install gnome-terminal
 
 #run flask
 export FLASK_APP=server_api_instance.py
