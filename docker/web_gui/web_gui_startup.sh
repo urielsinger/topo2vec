7b50db75b579777261e59d085b5b86c3ce5a6928
Jonathan Zarecki <yonilx@gmail.com>
2020-04-27 11:00:58 +0300

    some necceessary data

--- docker/web_gui/web_gui_startup.sh
+++ docker/web_gui/web_gui_startup.sh
@@ -1,2 +1,3 @@
-cd /home/root/server_api/visualizations
-bokeh serve --address=0.0.0.0 --port=5432 --allow-websocket-origin=0.0.0.0:5432
+#cd /home/root/server_api/visualizations/bokeh_server
+cd /home/topo2vec_kavitzky/topo2vec/server_api/visualizations/bokeh_server
+bokeh serve . --address=0.0.0.0 --port=5432 --allow-websocket-origin=0.0.0.0:5432
