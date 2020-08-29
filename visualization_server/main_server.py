a2d1dee2444f22f46dc6033ee808040480c33ce3
Jonathan Zarecki <yonilx@gmail.com>
2020-08-29 21:50:39 +0300

    run the code on all the data

--- visualization_server/main_server.py
+++ visualization_server/main_server.py
@@ -7,7 +7,7 @@ from topo2vec.data_handlers.data_from_file_handler import DataFromFileHandler
 ELEVATION_BASE_DIR1 = BASE_LOCATION + 'data/elevation/big_europe'
 ELEVATION_BASE_DIR2 = BASE_LOCATION + 'data/elevation/mid_east'
 MASK_BASE_DIR = BASE_LOCATION + 'data/elevation/45,5,50,15'
-boxes = [(5, 45, 15, 49)]#, (33, 30, 37, 32), (35, 31, 36, 35)]
+boxes = [(5, 45, 15, 50)]#, (33, 30, 37, 32), (35, 31, 36, 35)]
 
 base_dirs = [ELEVATION_BASE_DIR1, ELEVATION_BASE_DIR2, ELEVATION_BASE_DIR2, ELEVATION_BASE_DIR2]
 data_visualizer = DataFromFileHandler(base_dirs, boxes)
